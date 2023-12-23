f0135b5b65858883c7809f7fbba945fe0406caaa
nezhinskii <maksimnezhin@gmail.com>
2023-12-23 16:13:06 +0300

    $&$@O!&(#&!@#^$%!(*

---
+++ ClipsFormsExample.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace ClipsFormsExample
+{
+    partial class ClipsFormsExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClipsFormsExample));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.codeBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.languagePropertyConfidenceTB = new System.Windows.Forms.TextBox();
+            this.languagePropertyPick = new System.Windows.Forms.Button();
+            this.languagePropertyDropdown = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.requirementConfidenceTB = new System.Windows.Forms.TextBox();
+            this.typeConfidenceTB = new System.Windows.Forms.TextBox();
+            this.requirementPick = new System.Windows.Forms.Button();
+            this.requirementDropdown = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.typePick = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.typeDropdown = new System.Windows.Forms.ComboBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.clipsOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.clipsSaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1329, 632);
+            this.panel1.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.codeBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.outputBox);
+            this.splitContainer1.Size = new System.Drawing.Size(1329, 632);
+            this.splitContainer1.SplitterDistance = 779;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // codeBox
+            // 
+            this.codeBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.codeBox.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.codeBox.Location = new System.Drawing.Point(0, 0);
+            this.codeBox.Multiline = true;
+            this.codeBox.Name = "codeBox";
+            this.codeBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.codeBox.Size = new System.Drawing.Size(779, 632);
+            this.codeBox.TabIndex = 2;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputBox.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputBox.Location = new System.Drawing.Point(0, 0);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(546, 632);
+            this.outputBox.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.languagePropertyConfidenceTB);
+            this.panel2.Controls.Add(this.languagePropertyPick);
+            this.panel2.Controls.Add(this.languagePropertyDropdown);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.requirementConfidenceTB);
+            this.panel2.Controls.Add(this.typeConfidenceTB);
+            this.panel2.Controls.Add(this.requirementPick);
+            this.panel2.Controls.Add(this.requirementDropdown);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.typePick);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.typeDropdown);
+            this.panel2.Controls.Add(this.nextButton);
+            this.panel2.Controls.Add(this.resetButton);
+            this.panel2.Controls.Add(this.openButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 586);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1331, 100);
+            this.panel2.TabIndex = 6;
+            // 
+            // languagePropertyConfidenceTB
+            // 
+            this.languagePropertyConfidenceTB.Location = new System.Drawing.Point(709, 68);
+            this.languagePropertyConfidenceTB.Name = "languagePropertyConfidenceTB";
+            this.languagePropertyConfidenceTB.Size = new System.Drawing.Size(42, 20);
+            this.languagePropertyConfidenceTB.TabIndex = 21;
+            this.languagePropertyConfidenceTB.Text = "1.0";
+            this.languagePropertyConfidenceTB.Visible = false;
+            // 
+            // languagePropertyPick
+            // 
+            this.languagePropertyPick.Location = new System.Drawing.Point(759, 67);
+            this.languagePropertyPick.Name = "languagePropertyPick";
+            this.languagePropertyPick.Size = new System.Drawing.Size(75, 23);
+            this.languagePropertyPick.TabIndex = 20;
+            this.languagePropertyPick.Text = "Добавить";
+            this.languagePropertyPick.UseVisualStyleBackColor = true;
+            this.languagePropertyPick.Visible = false;
+            this.languagePropertyPick.Click += new System.EventHandler(this.languagePropertyPick_Click);
+            // 
+            // languagePropertyDropdown
+            // 
+            this.languagePropertyDropdown.FormattingEnabled = true;
+            this.languagePropertyDropdown.Items.AddRange(new object[] {
+            "Компилируемый",
+            "Интерпретируемый",
+            "Статическая типизация",
+            "Динамическая типизация",
+            "ООП",
+            "ФП"});
+            this.languagePropertyDropdown.Location = new System.Drawing.Point(563, 67);
+            this.languagePropertyDropdown.Name = "languagePropertyDropdown";
+            this.languagePropertyDropdown.Size = new System.Drawing.Size(140, 21);
+            this.languagePropertyDropdown.TabIndex = 19;
+            this.languagePropertyDropdown.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(502, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Свойства";
+            this.label3.Visible = false;
+            // 
+            // requirementConfidenceTB
+            // 
+            this.requirementConfidenceTB.Location = new System.Drawing.Point(709, 41);
+            this.requirementConfidenceTB.Name = "requirementConfidenceTB";
+            this.requirementConfidenceTB.Size = new System.Drawing.Size(42, 20);
+            this.requirementConfidenceTB.TabIndex = 17;
+            this.requirementConfidenceTB.Text = "1.0";
+            // 
+            // typeConfidenceTB
+            // 
+            this.typeConfidenceTB.Location = new System.Drawing.Point(346, 40);
+            this.typeConfidenceTB.Name = "typeConfidenceTB";
+            this.typeConfidenceTB.Size = new System.Drawing.Size(42, 20);
+            this.typeConfidenceTB.TabIndex = 16;
+            this.typeConfidenceTB.Text = "1.0";
+            // 
+            // requirementPick
+            // 
+            this.requirementPick.Location = new System.Drawing.Point(759, 40);
+            this.requirementPick.Name = "requirementPick";
+            this.requirementPick.Size = new System.Drawing.Size(75, 23);
+            this.requirementPick.TabIndex = 15;
+            this.requirementPick.Text = "Добавить";
+            this.requirementPick.UseVisualStyleBackColor = true;
+            this.requirementPick.Click += new System.EventHandler(this.requirementPick_Click);
+            // 
+            // requirementDropdown
+            // 
+            this.requirementDropdown.FormattingEnabled = true;
+            this.requirementDropdown.Items.AddRange(new object[] {
+            "Быстродействие",
+            "Быстрое прототипирование",
+            "Популярность технологии",
+            "Широкая кастомизация",
+            "Решение из коробки",
+            "Предсказуемость"});
+            this.requirementDropdown.Location = new System.Drawing.Point(563, 40);
+            this.requirementDropdown.Name = "requirementDropdown";
+            this.requirementDropdown.Size = new System.Drawing.Size(140, 21);
+            this.requirementDropdown.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(490, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Требования";
+            // 
+            // typePick
+            // 
+            this.typePick.Location = new System.Drawing.Point(394, 39);
+            this.typePick.Name = "typePick";
+            this.typePick.Size = new System.Drawing.Size(75, 23);
+            this.typePick.TabIndex = 12;
+            this.typePick.Text = "Добавить";
+            this.typePick.UseVisualStyleBackColor = true;
+            this.typePick.Click += new System.EventHandler(this.typePick_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Тип проекта";
+            // 
+            // typeDropdown
+            // 
+            this.typeDropdown.FormattingEnabled = true;
+            this.typeDropdown.Items.AddRange(new object[] {
+            "Сайт",
+            "Сервер",
+            "Data science",
+            "Игра",
+            "Мобильное приложение",
+            "Desktop"});
+            this.typeDropdown.Location = new System.Drawing.Point(225, 39);
+            this.typeDropdown.Name = "typeDropdown";
+            this.typeDropdown.Size = new System.Drawing.Size(115, 21);
+            this.typeDropdown.TabIndex = 10;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nextButton.Location = new System.Drawing.Point(1199, 34);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(120, 30);
+            this.nextButton.TabIndex = 8;
+            this.nextButton.Text = "Дальше";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetButton.Location = new System.Drawing.Point(1046, 35);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(120, 30);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Рестарт";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 35);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(120, 30);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "Открыть";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // clipsOpenFileDialog
+            // 
+            this.clipsOpenFileDialog.Filter = "TXT files|*.txt|All files|*.*";
+            this.clipsOpenFileDialog.Title = "Открыть файл кода CLIPS";
+            // 
+            // clipsSaveFileDialog
+            // 
+            this.clipsSaveFileDialog.Filter = "CLIPS files|*.clp|All files|*.*";
+            this.clipsSaveFileDialog.Title = "Созранить файл как...";
+            // 
+            // ClipsFormsExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1331, 686);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(660, 300);
+            this.Name = "ClipsFormsExample";
+            this.Text = "Экспертная система \"Стек проекта\"";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.TextBox codeBox;
+    private System.Windows.Forms.TextBox outputBox;
+    private System.Windows.Forms.Panel panel2;
+    private System.Windows.Forms.Button nextButton;
+    private System.Windows.Forms.Button resetButton;
+    private System.Windows.Forms.Button openButton;
+    private System.Windows.Forms.OpenFileDialog clipsOpenFileDialog;
+    private System.Windows.Forms.FontDialog fontDialog1;
+    private System.Windows.Forms.SaveFileDialog clipsSaveFileDialog;
+        private System.Windows.Forms.ComboBox typeDropdown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button typePick;
+        private System.Windows.Forms.Button requirementPick;
+        private System.Windows.Forms.ComboBox requirementDropdown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox typeConfidenceTB;
+        private System.Windows.Forms.TextBox requirementConfidenceTB;
+        private System.Windows.Forms.TextBox languagePropertyConfidenceTB;
+        private System.Windows.Forms.Button languagePropertyPick;
+        private System.Windows.Forms.ComboBox languagePropertyDropdown;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ClipsFormsExample.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CLIPSNET;
+
+
+namespace ClipsFormsExample
+{
+    public partial class ClipsFormsExample : Form
+    {
+        private CLIPSNET.Environment clips = new CLIPSNET.Environment();
+        /// <summary>
+        /// Распознаватель речи
+        /// </summary>
+        //private Microsoft.Speech.Synthesis.SpeechSynthesizer synth;
+        
+        /// <summary>
+        /// Распознавалка
+        /// </summary>
+        //private Microsoft.Speech.Recognition.SpeechRecognitionEngine recogn;
+        private List<string> ruleConfidenceFacts = new List<string>();
+        public ClipsFormsExample()
+        {
+            InitializeComponent();
+            typeDropdown.SelectedIndex = 0;
+            requirementDropdown.SelectedIndex = 0;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        private void HandleResponse()
+        {
+            //  Вытаскиаваем факт из ЭС
+            String evalStr = "(find-fact ((?f ioproxy)) TRUE)";
+            FactAddressValue fv = (FactAddressValue)((MultifieldValue)clips.Eval(evalStr))[0];
+
+            MultifieldValue damf = (MultifieldValue)fv["messages"];
+            MultifieldValue vamf = (MultifieldValue)fv["answers"];
+
+            outputBox.Text += "Новая итерация : " + System.Environment.NewLine;
+            for (int i = 0; i < damf.Count; i++)
+            {
+                LexemeValue da = (LexemeValue)damf[i];
+                byte[] bytes = Encoding.Default.GetBytes(da.Value);
+                string message = Encoding.UTF8.GetString(bytes);
+                //synth.SpeakAsync(message);
+                outputBox.Text += message + System.Environment.NewLine;
+            }
+           
+            
+            if(vamf.Count == 0)
+                clips.Eval("(assert (clearmessage))");
+        }
+
+        private void print_facts()
+        {
+            foreach (var fact in clips.GetFactList())
+            {
+                StringBuilder factData = new StringBuilder($"({fact.RelationName} ");
+                fact.GetSlotValues().ForEach(v => { factData.Append($"({v.SlotName} {v.Contents})"); });
+                factData.Append(')');
+                Console.WriteLine(factData.ToString());
+            }
+            Console.WriteLine("------------------------------------------------------------------");
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            clips.Run();
+            HandleResponse();
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = "Выполнены команды Clear и Reset." + System.Environment.NewLine;
+            //  Здесь сохранение в файл, и потом инициализация через него
+            clips.Clear();
+
+            //  Так тоже можно - без промежуточного вывода в файл
+            clips.LoadFromString(codeBox.Text);
+
+            clips.Reset();
+
+            foreach(var fact in ruleConfidenceFacts)
+            {
+                clips.Eval(fact);
+            }
+        }
+
+        String map_fact_number_to_group(String fact)
+        {
+            int number = int.Parse(fact.Substring(1));
+            if (number < 200)
+            {
+                return "type";
+            } else if (number < 300)
+            {
+                return "requirement";
+            } else if (number < 400)
+            {
+                return "platform";
+            } else if (number < 500)
+            {
+                return "language";
+            } else if (number < 600)
+            {
+                return "framework";
+            } else
+            {
+                return "language_property";
+            }
+        }
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if (clipsOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Dictionary<String, String> facts = new Dictionary<String, String>();
+                int ruleNum = 0;
+                StringBuilder database = new StringBuilder();
+                database.Append(System.IO.File.ReadAllText("../../common_rules_2.0.txt"));
+                foreach (String line in System.IO.File.ReadAllLines(clipsOpenFileDialog.FileName))
+                {
+                    if (line.Contains(':'))
+                    {
+                        String[] parts = line.Split(':');
+                        facts[parts[0].Trim()] = parts[1].Trim();
+                    }
+                    else if (line.Contains('='))
+                    {
+                        String[] ruleConf = line.Split(';');
+                        String[] premiseResult = ruleConf[0].Split('=');
+                        String resultGroup = map_fact_number_to_group(premiseResult[1].Trim());
+                        double ruleConfidence = 0.9;
+                        if (ruleConf.Length > 1 && ruleConf[1].Trim().Length > 0)
+                        {
+                            ruleConfidence = double.Parse(ruleConf[1].Trim(), NumberStyles.Any, CultureInfo.InvariantCulture);
+                        }
+                        StringBuilder defrule = new StringBuilder($"(defrule rule{ruleNum}\r\n");
+                        int salience = int.Parse(premiseResult[1].Trim()[1].ToString());
+                        if (salience == 6 || salience < 3)
+                        {
+                            salience = 0;
+                        }
+                        defrule.Append($"\t(declare (salience {salience}))\r\n");
+                        defrule.Append($"\t(rule_confidence (ruleNumber {ruleNum}) (confidence {ruleConfidence.ToString(CultureInfo.InvariantCulture)}))\r\n");
+                        int i = 1;
+                        foreach (String premise in premiseResult[0].Split(','))
+                        {
+                            String premiseGroup = map_fact_number_to_group(premise.Trim());
+                            defrule.Append($"\t(fact_with_confidence (fact \"{facts[premise.Trim()]}\") (confidence ?f{i}_confidence))\r\n");
+                            i++;
+                        }
+                        defrule.Append($"=>\r\n");
+                        defrule.Append($"\t(bind ?min_fact_confidence (min 1.0");
+                        i--;
+                        while (i >= 1)
+                        {
+                            defrule.Append($" ?f{i}_confidence");
+                            i--;
+                        }
+                        defrule.Append("))\r\n");
+                        defrule.Append($"\t(bind ?res_confidence (* ?min_fact_confidence {ruleConfidence.ToString(CultureInfo.InvariantCulture)}))\r\n");
+                        defrule.Append($"\t(assert (fact_with_confidence (fact \"{facts[premiseResult[1].Trim()]}\") (confidence ?res_confidence)))\r\n");
+                        defrule.Append($"\t(assert (sendmessagehalt \"Выведено: {facts[premiseResult[1].Trim()]} с уверенностью \" (str-cat ?res_confidence)))\r\n)\r\n");
+                        database.Append(defrule);
+                        ruleConfidenceFacts.Add($"(assert (rule_confidence (ruleNumber {ruleNum}) (confidence {ruleConfidence.ToString(CultureInfo.InvariantCulture)})))");
+                        ruleNum += 1;
+                    }
+                }
+                codeBox.Text = database.ToString();
+                //codeBox.Text = System.IO.File.ReadAllText(clipsOpenFileDialog.FileName);
+                Text = "Экспертная система \"Стек проекта\" – " + clipsOpenFileDialog.FileName;
+            }
+        }
+        private void typePick_Click(object sender, EventArgs e)
+        {
+            string type = typeDropdown.GetItemText(typeDropdown.Items[typeDropdown.SelectedIndex]);
+            clips.Eval($"(assert (add-fact (fact \"{type}\") (confidence {typeConfidenceTB.Text})))");
+            clips.Run();
+            print_facts();
+            clips.Run();
+            print_facts();
+            HandleResponse();
+        }
+
+        private void requirementPick_Click(object sender, EventArgs e)
+        {
+            string requirement = requirementDropdown.GetItemText(requirementDropdown.Items[requirementDropdown.SelectedIndex]);
+            clips.Eval($"(assert (add-fact (fact \"{requirement}\") (confidence {requirementConfidenceTB.Text})))");
+            clips.Run();
+            print_facts();
+            clips.Run();
+            print_facts();
+            HandleResponse();
+        }
+
+        private void languagePropertyPick_Click(object sender, EventArgs e)
+        {
+            string property = languagePropertyDropdown.GetItemText(languagePropertyDropdown.Items[languagePropertyDropdown.SelectedIndex]);
+            clips.Eval($"(assert (add-fact (fact \"{property}\") (confidence {languagePropertyConfidenceTB.Text})))");
+            clips.Run();
+            print_facts();
+            clips.Run();
+            print_facts();
+            HandleResponse();
+        }
+    }
+}
